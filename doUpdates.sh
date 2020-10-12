b337f62287a8821eeeb327db380f22ba54d91657
thelowtech <novar34@Gmail.com>
2020-10-12 08:55:59 -0500

    Update doUpdates.sh

--- doUpdates.sh
+++ doUpdates.sh
@@ -2,8 +2,8 @@ sudo apt clean
 sudo apt update
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
-sudo apt autoremove -y
 sudo apt autoclean -y
+sudo apt autoremove --purge -y
 
 if [ -f /var/run/reboot-required ]; then
   sudo shutdown -r now
