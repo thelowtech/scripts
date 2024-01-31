5d8edc4ed8be9de7d797f0a0d49dcb0e68d2e24a
thelowtech <novar34@Gmail.com>
2024-01-31 17:32:07 -0600

    Update updateHalt.sh

--- updateHalt.sh
+++ updateHalt.sh
@@ -1,8 +1,8 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo apt clean
 sudo apt update
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
+sudo apt DEBIAN_FRONTEND=noninteractive upgrade -y
+sudo apt DEBIAN_FRONTEND=noninteractive dist-upgrade -y
 sudo apt autoclean -y
 sudo apt autoremove --purge -y
 sudo shutdown -h now
