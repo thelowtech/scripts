be839630842ec8dbff344eaf92afcbb346f8e2a3
thelowtech <novar34@Gmail.com>
2025-06-30 23:07:57 -0500

    Update updateHalt.sh

--- updateHalt.sh
+++ updateHalt.sh
@@ -1,7 +1,7 @@
 sudo apt clean
 sudo apt update
-sudo apt DEBIAN_FRONTEND=noninteractive upgrade -y
-sudo apt DEBIAN_FRONTEND=noninteractive dist-upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt dist-upgrade -y
 sudo apt autoclean -y
 sudo apt autoremove --purge -y
 sudo shutdown -h now
